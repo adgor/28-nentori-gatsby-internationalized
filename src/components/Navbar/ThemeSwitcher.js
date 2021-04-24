f593c55303aa1017af1553d20578b7a75ccadda0
adgor <adgor000@gmail.com>
2021-04-24 16:11:20 +0200

    added heroicons & navbar

---
+++ src/components/Navbar/ThemeSwitcher.js
@@ -0,0 +1,18 @@
+import React from "react";
+import useDarkMode from "../../hook/useDarkMode";
+import { MoonIcon, SunIcon } from "@heroicons/react/outline";
+
+export default function ThemeSwitcher() {
+  const [colorTheme, setTheme] = useDarkMode();
+  return (
+    <>
+      <span className="ml-3 " onClick={() => setTheme(colorTheme)}>
+        {colorTheme === "light" ? (
+          <SunIcon className="w-5 h-5 text-scooter-500 " />
+        ) : (
+          <MoonIcon className="w-5 h-5 text-paleSky-900" />
+        )}
+      </span>
+    </>
+  );
+}
