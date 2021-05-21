695b9c16560bfdbf6128ae3d086eab240466e046
adgor <adgor000@gmail.com>
2021-05-21 11:08:21 +0200

    fixed react heroicon

--- src/components/sections/styles/FeaturedP.js
+++ src/components/sections/styles/FeaturedP.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { CheckIcon } from "@heroicons/react/outline";
+import { HiCheck } from "react-icons/hi";
 
 export default function FeaturedP(props) {
   return (
     <p className="flex items-center text-base font-normal leading-relaxed lg:pr-10 text-paleSky-500 dark:text-paleSky-200">
-      <CheckIcon className="w-6 h-6 mr-2 text-biscay-500 dark:text-white " />
+      <HiCheck className="w-6 h-6 mr-2 text-biscay-500 dark:text-white " />
       {props.text}
     </p>
   );
