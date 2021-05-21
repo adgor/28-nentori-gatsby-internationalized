10e364b9d54ed65d14faedaf26d5a57ea5ce2738
adgor <adgor000@gmail.com>
2021-05-21 12:05:21 +0200

    fixed reacit icons

--- src/components/StatsItem.js
+++ src/components/StatsItem.js
@@ -4,12 +4,12 @@ export const StatsItem = ({ num, text, statIcon }) => {
   return (
     <div className="flex flex-col sm:flex-row">
       <div className="sm:mr-4">
-        <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+        <div>
           {statIcon}
         </div>
       </div>
       <div>
-        <h6 className="mb-2 text-4xl font-bold leading-5">{num}</h6>
+        <h6 className="mb-2 mt-1 text-4xl font-bold leading-5">{num}</h6>
         <p className="mb-3 text-base text-gray-900">{text}</p>
       </div>
     </div>
